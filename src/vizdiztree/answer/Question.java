f38207d1b5712b6d1ab580636fb43d8d13e8d9a0
TaylorJesse <taylor.jesse@colorado.edu>
2018-03-19 16:15:27 -0600

    Temporary Initial Question class

--- src/vizdiztree/answer/Question.java
+++ src/vizdiztree/answer/Question.java
@@ -1,4 +1,36 @@
 package vizdiztree.answer;
+import java.util.*;
 
 public class Question {
+	private String title;
+	private ArrayList<Answer> lista;
+
+	public Question(String _title, ArrayList<Answer> _lista){
+		setTitle(_title);
+		addAnswers(_lista);
+	}
+
+	public void setTitle(String _title){
+		this.title=_title;
+	}
+
+	public String getTitle(){
+		return this.title;
+	}
+
+	public void addAnswers(ArrayList<String> _lista){
+		// for(String a: _lista){
+			
+		// }
+	}
+
+	public ArrayList<Answer> getAnswers(){
+		return this.lista;
+	}
+
+	//TODO: Need to check specific conversion details for desicion tree.
+	public Question convertQuestion(){
+		return null;
+	}
+
 }
