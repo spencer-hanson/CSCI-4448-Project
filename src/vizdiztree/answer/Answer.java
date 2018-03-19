15fee5f9b5c9a8ec52ed6bc1adedcf9058fcc7b7
TaylorJesse <taylor.jesse@colorado.edu>
2018-03-19 15:56:08 -0600

    Initial Answer class- Everything done but conversion

--- src/vizdiztree/answer/Answer.java
+++ src/vizdiztree/answer/Answer.java
@@ -1,4 +1,22 @@
 package vizdiztree.answer;
 
 public class Answer {
+	private String title;
+
+	public Answer(String _title){
+		setTitle(_title);
+	}
+
+	public void setTitle(String _title){
+		this.title=_title;
+	}
+
+	public String getTitle(){
+		return this.title;
+	}
+
+	//TODO: Need to check specific conversion details for desicion tree.
+	public Answer convertAnswer(){
+		return null;
+	}
 }
