4685f286240bf3e6742a58f5a02c4ab0bc06db27
TaylorJesse <taylor.jesse@colorado.edu>
2018-03-19 19:46:05 -0600

    Some Test Cases for Answer Class

--- src/vizdiztree/Main.java
+++ src/vizdiztree/Main.java
@@ -1,7 +1,14 @@
 package vizdiztree;
+import vizdiztree.answer.Answer;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        //Test Answer class methods
+        Answer ans= new Answer("Yes");
+        System.out.println(ans.getTitle());
+        ans.setTitle("No");
+        System.out.println(ans.getTitle());
     }
 }
