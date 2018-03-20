2d93751a96e956695dc93cac1664c355aa6c9183
TaylorJesse <taylor.jesse@colorado.edu>
2018-03-20 17:00:24 -0600

    Possible method to generate initial survey response data

--- src/vizdiztree/survey/Survey.java
+++ src/vizdiztree/survey/Survey.java
@@ -2,7 +2,7 @@ package vizdiztree.survey;
 
 import vizdiztree.answer.*;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Survey {
     private String title;
@@ -31,6 +31,26 @@ public class Survey {
 
     public ArrayList<SurveyResponse> getResponses() {
         return responses;
+    }
+
+    public ArrayList<SurveyResponse> takeSurvey(){
+        int i=0;
+
+        ArrayList<SurveyResponse> r=new ArrayList<>();
+        while(i<10) {
+            for (Question q : questions) {
+                Random rand = new Random();
+                ArrayList<Answer> alist = q.getAnswers();
+                int index = rand.nextInt(alist.size());
+                Answer a = alist.get(index);
+                SurveyResponse newR= r.get(i);
+                newR.addResponse(a.getTitle());
+                r.set(i,newR);
+            }
+            i = i + 1;
+        }
+        return r;
+
     }
 
     //TODO: Need to check specific conversion details for decision tree.
