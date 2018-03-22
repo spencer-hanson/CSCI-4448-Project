bcf4da8aad73cefa89283875b24e58366b6a0907
Sam Reed <samdreed21@gmail.com>
2018-03-22 00:16:10 -0600

    Need to integrate maven project with intellij

---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+import quickdt.*;
+public class App
+{
+	//private void 
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello" );
+    }
+}
