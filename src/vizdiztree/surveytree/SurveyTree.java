d20c91bd853f84ecaa9fd601ac169dc5fecaf57e
Sam Reed <samdreed21@gmail.com>
2018-04-13 11:05:52 -0600

    Survey tree class added

--- src/vizdiztree/surveytree/SurveyTree.java
+++ src/vizdiztree/surveytree/SurveyTree.java
@@ -1,15 +1,22 @@
-  
-  public int buildTree(){
-        try{
-                ProcessBuilder pb = new ProcessBuilder("python","decision_tree.py");
+package vizdiztree.surveytree;
+import vizdiztree.answer.*;
+import java.util.*;
+import java.io.*;
+
+public class SurveyTree {
+    public int buildTree() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder("python", "decision_tree.py");
             Process p = pb.start();
 
             BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
             int ret = new Integer(in.readLine()).intValue();
             System.out.println(ret);
 
-        }catch(Exception e){System.out.println(e);}
-
-
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         return 0;
     }
+
+}
