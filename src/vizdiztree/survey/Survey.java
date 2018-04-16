f5ee64a23e18507fe7b7191269c3263a07eb2254
Spencer Hanson <spencer.hanson@swimlane.com>
2018-04-16 17:12:24 -0600

    Added stuff

--- src/vizdiztree/survey/Survey.java
+++ src/vizdiztree/survey/Survey.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class Survey {
     private String title;
     private ArrayList<SurveyResponse> responses= new ArrayList<>();
-    private ArrayList<ArrayList<Integer>> convertedResponses=new ArrayList<>();
+    public ArrayList<ArrayList<Integer>> convertedResponses=new ArrayList<>();
 
     private ArrayList<Question> questions= new ArrayList<>();
     private HashMap<String, Integer> keys= new HashMap<>();
