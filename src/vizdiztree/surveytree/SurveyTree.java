33a6fdd31e03306823c363bff7720039ec32c8ac
Sam Reed <samdreed21@gmail.com>
2018-04-16 15:09:19 -0600

    Writes text file and calls python decision tree script

--- src/vizdiztree/surveytree/SurveyTree.java
+++ src/vizdiztree/surveytree/SurveyTree.java
@@ -1,12 +1,38 @@
 package vizdiztree.surveytree;
+//import org.supercsv.cellprocessor.ift.CellProcessor;
 import vizdiztree.answer.*;
+import vizdiztree.survey.*;
 import java.util.*;
 import java.io.*;
 
+
 public class SurveyTree {
-    public int buildTree() {
+
+    public void writeData(ArrayList<ArrayList<Integer>> convertedresponses){
+        //write convertedResponses to a text file
         try {
-            ProcessBuilder pb = new ProcessBuilder("python", "testing.py");
+            PrintWriter writer = new PrintWriter("survey_data.txt", "UTF-8");
+            writer.println(convertedresponses);
+            writer.close();
+        }
+        catch (UnsupportedEncodingException fnfe) {
+            // do something sensible with the exception.
+        }
+        catch (FileNotFoundException fnfe) {
+            // do something sensible with the exception.
+        }
+    }
+//
+//    public void writeQuestions(){
+//        for(Question q: survey.getQuestions()){
+//            System.out.println(q.getTitle());
+//        }
+//    }
+
+
+    public void buildTree() {
+        try {
+            ProcessBuilder pb = new ProcessBuilder("python", "decision_tree.py");
             Process p = pb.start();
 
             BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -16,7 +42,6 @@ public class SurveyTree {
         } catch (Exception e) {
             System.out.println(e);
         }
-        return 0;
     }
 
 }
