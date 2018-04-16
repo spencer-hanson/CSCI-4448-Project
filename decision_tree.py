5951ec7649eb4d33f2dd45f180fb81201e82e581
Sam Reed <samdreed21@gmail.com>
2018-04-16 15:03:35 -0600

    decision_tree.py now makes predicitions based off text file created by surveytree class

--- decision_tree.py
+++ decision_tree.py
@@ -3,16 +3,10 @@ import graphviz
 import numpy as np
 import scipy
 import csv
+import ast
+import math
 
-# list of possible answers to the main question
-responses = {
-    "yes" : 5,
-    "no" : 6
-}
-
-reverse_responses = {}
-for k, v in responses.items():
-    reverse_responses[v] = k
+# TODO: Make questions read in from java program
 
 # list of questions that will be asked in decision making process
 questions = [
@@ -26,65 +20,44 @@ questions = [
 
 # parse the data from the csv into a list of lists, each list containing the results of one example
 
-def parse_date(filename):
-    with open(filename) as f:
-        rows = csv.reader(f)
-        results = list(rows)
-    return results
+
+def parse(filename):
+    with open(filename, 'r') as f:
+        mylist = ast.literal_eval(f.read())
+    return mylist
 
 
-# convert the data into numbers so that we can use the sklearn library to train the data
-
-#TODO Change conversions from AI problem set to conversions from Taylor's stuff
-
-def convert_data(data):
+def convert(data):
     response_data = []
     question_data = []
+    #  loop that reads from list of data (length n) and puts first n-1 entries into question data and last entrie
+    #  into question data
     for feature in data:
-        raw_features = []
-        for item in feature:
-            item = item.strip()
-            if item == "yes" or item == "no":
-                if item == "yes":
-                    raw_features.append(0)  # 0 for yes
-                else:
-                    raw_features.append(1)  # 1 for no
-            elif item == "short" or item == "medium" or item == "long":
-                if item == "short":
-                    raw_features.append(0)  # 0 for short
-                elif item == "medium":
-                    raw_features.append(1)  # 1 for medium
-                elif item == "long":
-                    raw_features.append(2)  # 2 for long
-            elif item == "0" or item == "some" or item == "3":
-                if item == "0":
-                    raw_features.append(0)  # 0 for 0 tickets
-                elif item == "some":
-                    raw_features.append(1)  # 1 for 1 or 2 tickets
-                elif item == "3":
-                    raw_features.append(2)  # 2 for 3+ tickets
-            elif item == "ans:yes" or item == "ans:no":  # answer
-                response_data.append(responses[item.split(':')[1]])
-        question_data.append(raw_features)
-    return {"x": question_data, "y": response_data}  # x for questiondate y for response data (answers)
+        raw_data = []
+        count = 0
+        while count < len(feature)-1:
+            raw_data.append(feature[count])
+            count += 1
+        response_data.append(feature[len(feature)-1])
+        question_data.append(raw_data)
+
+    return {"x": question_data, "y": response_data}  # x for question data y for response data (answers)
 
 
 if __name__ == "__main__":
+    newparse = parse("survey_data.txt")
+    num_items = math.floor(len(newparse)*.75)  # take 75% of entries for training and 25% for testing
+    converted_new = convert(newparse)
 
-    data = "fixed_examples.csv"
-    all_data = parse_date(data)  # not converted data, just data parsed from file
+    perm = np.random.permutation(len(newparse))  # permutation for shuffling data
+    converted_new['x'] = list(np.array(converted_new['x'])[perm])
+    converted_new['y'] = list(np.array(converted_new['y'])[perm])
 
-    converted_data = convert_data(all_data) # convert the data parsed into just numbers
+    trainx = converted_new['x'][:num_items]  # grab 20 example sets that have been converted
+    trainy = converted_new['y'][:num_items]  # grab the response data that's been converted and corresponds to the examples
 
-    perm = np.random.permutation(25)  # permutation for shuffling data
-    converted_data['x'] = list(np.array(converted_data['x'])[perm])
-    converted_data['y'] = list(np.array(converted_data['y'])[perm])
-
-    trainx = converted_data['x'][:20]  # grab 20 example sets that have been converted
-    trainy = converted_data['y'][:20]  # grab the response data that's been converted and corresponds to the examples
-
-    testx = converted_data['x'][20:]  # grab the remaining for testing
-    testy = converted_data['x'][20:]
+    testx = converted_new['x'][num_items:]  # grab the remaining for testing
+    testy = converted_new['x'][num_items:]
 
     clf = tree.DecisionTreeClassifier()  # use the library function to create the decision tree
     clf = clf.fit(trainx, trainy)  # run our training data
@@ -93,7 +66,7 @@ if __name__ == "__main__":
     print(predictions)
 
 
-# use graphiz to create a graph representing the decision tree
+# use graphviz to create a graph representing the decision tree
  #   dot_data = tree.export_graphviz(clf, out_file=None,
   #                                  feature_names=questions,
    #                                class_names=list(responses.keys()),
